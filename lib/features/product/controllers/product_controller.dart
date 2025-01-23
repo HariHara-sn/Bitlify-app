266c303d5f5a456a718117ae08a7579d00a5cedd
HariHara-sn <sudhanabirami007@gmail.com>
2025-01-23 21:32:13 +0530

    updated Themes

--- lib/features/product/controllers/product_controller.dart
+++ lib/features/product/controllers/product_controller.dart
@@ -2,7 +2,7 @@ import '../models/product_model.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:bike_shoping_app/core/constants/images.dart';
 
-// Provider for fetching product data
+
 final productProvider = FutureProvider<List<ProductModel>>((ref) async {
   await Future.delayed(const Duration(seconds: 2));
 
